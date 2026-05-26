842e4c6dec100e5539173512f2d05d5d5f6fa10e
saintpa <parham.m1382@yahoo.com>
2026-05-26 19:21:46 +0330

    Improve audio channel handling

--- audio/audio_engine.py
+++ audio/audio_engine.py
@@ -1,7 +1,10 @@
 import pygame
 
+pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512)
 
 pygame.mixer.init()
+
+pygame.mixer.set_num_channels(64)
 
 
 def load_sound(path):
@@ -10,4 +13,7 @@ def load_sound(path):
 
 def play_sound(sound):
     if sound:
-        sound.play()
+        channel = pygame.mixer.find_channel(force=True)
+        if channel:
+            channel.play(sound)
+
